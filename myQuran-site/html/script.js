e6529597dabbc78479769d62b35ae0b5e6d755ee
Tomtiko04 <83877877+Tomtiko04@users.noreply.github.com>
2022-08-06 23:00:19 +0100

    Update script.js

--- myQuran-site/html/script.js
+++ myQuran-site/html/script.js
@@ -1,1 +1,10 @@
-
+ const search = document.querySelector('#search-btn');
+ const searchBox = document.querySelector('.search-form');
+ search.addEventListener('click', function() {
+       searchBox.classList.remove('hidden');
+    });
+ document.addEventListener('keydown', function(e) {
+            if (e.key === 'Enter') {
+                searchBox.classList.add('hidden');
+            }
+        });
